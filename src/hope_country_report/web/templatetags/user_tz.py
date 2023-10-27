5e4e8cdb13c97239596acbddbae6ecd9e16896d4
sax <s.apostolico@gmail.com>
2023-10-27 12:37:11 +0200

    updates tests

--- src/hope_country_report/web/templatetags/user_tz.py
+++ src/hope_country_report/web/templatetags/user_tz.py
@@ -1,30 +1,30 @@
 from datetime import datetime
 
+from django.contrib.auth.models import AnonymousUser
 from django.template import Library
 from django.template.defaultfilters import date
 
 from hope_country_report.apps.core.models import User
+from hope_country_report.state import state
 
 register = Library()
 
 
 @register.filter(expects_localtime=True, is_safe=False)
-def userdatetime(value: datetime, user: "User|None" = None, arg: str = "Y M d H:i") -> str:
-    if user:
-        try:
-            tz = user.timezone
-            value = value.astimezone(tz)
-        except ValueError:
-            pass
+def userdatetime(value: datetime, user: "User|AnonymousUser|None" = None, arg: str = "Y M d H:i") -> str:
+    if not user:
+        user = state.request.user
+    if user.is_authenticated and user.timezone:
+        tz = user.timezone
+        value = value.astimezone(tz)
     return date(value, arg)
 
 
 @register.filter(expects_localtime=True, is_safe=False)
-def usertime(value: datetime, user: "User|None" = None, arg: str = "H:i") -> str:
-    if user:
-        try:
-            tz = user.timezone
-            value = value.astimezone(tz)
-        except ValueError:
-            pass
+def usertime(value: datetime, user: "User|AnonymousUser|None" = None, arg: str = "H:i") -> str:
+    if not user:
+        user = state.request.user
+    if user.is_authenticated and user.timezone:
+        tz = user.timezone
+        value = value.astimezone(tz)
     return date(value, arg)
--- tests/web/templatetags/test_tags_user_tz.py
+++ tests/web/templatetags/test_tags_user_tz.py
@@ -1,15 +1,43 @@
 import zoneinfo
 
+from unittest.mock import Mock
+
+from django.contrib.auth.models import AnonymousUser
 from django.utils.timezone import now
 
 import freezegun
 
-from hope_country_report.web.templatetags.user_tz import userdatetime
+from hope_country_report.state import state
+from hope_country_report.web.templatetags.user_tz import userdatetime, usertime
 
 
 @freezegun.freeze_time("2000-01-01 08:00:00 UTC")
 def test_userdatetime(user):
     user.timezone = zoneinfo.ZoneInfo("UTC")
     assert userdatetime(now(), user) == "2000 Jan 01 08:00"
+
     user.timezone = zoneinfo.ZoneInfo("Europe/Rome")
     assert userdatetime(now(), user) == "2000 Jan 01 09:00"
+
+    assert userdatetime(now(), AnonymousUser()) == "2000 Jan 01 08:00"
+
+    r = Mock()
+    r.user = user
+    with state.set(request=r):
+        assert userdatetime(now()) == "2000 Jan 01 09:00"
+
+
+@freezegun.freeze_time("2000-01-01 08:00:00 UTC")
+def test_usertime(user):
+    user.timezone = zoneinfo.ZoneInfo("UTC")
+    assert usertime(now(), user) == "08:00"
+
+    user.timezone = zoneinfo.ZoneInfo("Europe/Rome")
+    assert usertime(now(), user) == "09:00"
+
+    assert usertime(now(), AnonymousUser()) == "08:00"
+
+    r = Mock()
+    r.user = user
+    with state.set(request=r):
+        assert usertime(now()) == "09:00"
