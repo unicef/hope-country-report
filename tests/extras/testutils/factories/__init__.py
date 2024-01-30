dd3cc8bc99cc4b7826cd2daa7c0f239dbb9bf1ae
Domenico DiNicola <dom.dinicola@gmail.com>
2024-01-30 15:44:32 +0100

    fix factory

--- tests/extras/testutils/factories/__init__.py
+++ tests/extras/testutils/factories/__init__.py
@@ -1,4 +1,4 @@
-import typing
+from django.db.models.fields import UUIDField
 
 from .base import AutoRegisterModelFactory, factories_registry, HopeAutoRegisterModelFactory, TAutoRegisterModelFactory
 
@@ -19,7 +19,7 @@ def get_factory_for_model(_model) -> type[TAutoRegisterModelFactory]:
     bases = (AutoRegisterModelFactory,)
     if _model in factories_registry:
         return factories_registry[_model]
-    if _model._meta.app_label in ["hope"]:
+    if _model._meta.app_label in ["hope"] and isinstance(_model._meta.get_field("id"), UUIDField):
         bases = (HopeAutoRegisterModelFactory,)
 
     return type(f"{_model._meta.model_name}Factory", bases, {"Meta": Meta})
--- tests/extras/testutils/factories/base.py
+++ tests/extras/testutils/factories/base.py
@@ -21,9 +21,8 @@ class AutoRegisterModelFactory(factory.django.DjangoModelFactory, metaclass=Auto
 
 
 class HopeAutoRegisterModelFactory(AutoRegisterModelFactory):
-    # id = Faker("uuid4")
+    id = Faker("uuid4")
     # id = factory.LazyFunction(lambda: str(uuid.uuid4()))
-    pass
 
     # @classmethod
     # def create(cls, **kwargs):
