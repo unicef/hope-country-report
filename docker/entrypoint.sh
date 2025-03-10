753220029d1ace0c901c1fd589b0db05835a3302
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2025-03-10 12:15:13 +0100

    test fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,7 @@
 set -eou pipefail
 
 production() {
+    waitforit -address=tcp://db:5432
     uwsgi \
         --http :8000 \
         --master \
@@ -17,12 +18,12 @@ fi
 
 case "$1" in
     dev)
-        ./docker/wait-for-it.sh db:5432
+        waitforit -address=tcp://db:5432
         python3 manage.py migrate
         python3 manage.py runserver 0.0.0.0:8000
     ;;
     tests)
-        ./docker/wait-for-it.sh db:5432
+        waitforit -address=tcp://db:5432
         pytest tests/ --create-db --cov-report term --maxfail 5 --with-selenium
     ;;
     prd)
@@ -30,12 +31,14 @@ case "$1" in
     ;;
     celery_worker)
         export C_FORCE_ROOT=1
-        celery -A hope_country_report.config.celery worker -l info
+        watchmedo auto-restart --directory=./ --pattern=*.py --recursive -- celery -A hope_country_report.config.celery worker -l info
     ;;
     celery_beat)
+        waitforit -host=backend
         celery -A hope_country_report.config.celery beat -l info
     ;;
     celery_flower)
+        waitforit -host=backend
         celery -A hope_country_report.config.celery flower
     ;;
     *)
