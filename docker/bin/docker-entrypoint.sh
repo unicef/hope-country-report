5c891bd122b3810f753762597a4abd392670fbc5
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2025-07-10 15:39:22 +0200

    Permissions fixed

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -14,9 +14,7 @@ if [ -d "${STATIC_ROOT}" ]; then
 fi
 
 mkdir -p /app/
-if [ -d "/app/src" ]; then
-  chown -R hope:unicef /app/src
-fi
+chown -R hope:unicef /app
 cd /app
 
 case "$1" in
@@ -52,7 +50,8 @@ case "$1" in
         exec tini -- gosu hope:unicef celery -A hope_country_report.config.celery flower
         ;;
     run_tests)
-        exec tini -- gosu hope:unicef pytest tests/functional/test_f_crawl.py --create-db -n auto -v --maxfail=5 --migrations --cov-config=tests/.coveragerc --cov-report xml
+        exec tini -- gosu hope:unicef pytest tests/ --create-db -n auto -v --maxfail=5 --migrations
+--cov-config=tests/.coveragerc --cov-report xml:output/coverage.xml
 
         ;;
     *)
